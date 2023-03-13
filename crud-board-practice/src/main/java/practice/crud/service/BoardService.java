13a7102339faeab120b3d0585234cd2978cdd445
DevelopJJong <jjong___e@naver.com>
2023-03-13 17:33:50 +0900

    #6 - CRUD 기능구현
    getList 리스트를 불러오는 기능에는 내림차순으로 정리 추가하였다.

---
+++ crud-board-practice/src/main/java/practice/crud/service/BoardService.java
@@ -0,0 +1,57 @@
+package practice.crud.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import practice.crud.domain.Board;
+import practice.crud.repository.BoardRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    // @RequiredArgsConstructor 어노테이션을 이용해서 생성자를 만들어줌
+    private final BoardRepository boardRepository;
+
+    /**
+     * Create
+     */
+    public void create(Board board){
+        boardRepository.save(board);
+    }
+
+    /**
+     * List
+     * 게시글 id의 내림차순으로 정렬
+     */
+    public List<Board> getList(){
+        return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    /**
+     * READ
+     */
+    public Board detail(int id){
+        return boardRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * UPDATE
+     */
+    public void update(Board board){
+        boardRepository.save(board);
+    }
+
+    /**
+     * DELETE
+     */
+    public void delete(int id){
+        boardRepository.deleteById(id);
+    }
+
+
+}
