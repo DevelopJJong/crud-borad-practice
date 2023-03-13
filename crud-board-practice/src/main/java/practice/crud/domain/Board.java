c54cca71f3e90e35ac588126b25d863a10d86420
DevelopJJong <jjong___e@naver.com>
2023-03-13 17:29:46 +0900

    #3 - Board 엔티티를 만들었다.
    엔티티에 사용하는 어노테이션에 대해서 공부하였다.

---
+++ crud-board-practice/src/main/java/practice/crud/domain/Board.java
@@ -0,0 +1,39 @@
+package practice.crud.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private String hashtag;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+}
