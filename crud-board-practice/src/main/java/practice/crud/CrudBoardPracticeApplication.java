f60c898ba21de22e2b6959a90af1ef553dd5ef62
DevelopJJong <jjong___e@naver.com>
2023-03-13 15:12:14 +0900

    #2 - 디펜던시 구성 및 프로젝트 생성

---
+++ crud-board-practice/src/main/java/practice/crud/CrudBoardPracticeApplication.java
@@ -0,0 +1,13 @@
+package practice.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBoardPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBoardPracticeApplication.class, args);
+    }
+
+}
